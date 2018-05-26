36bea94e5adaf5e781357ac7882e3ed233c91821
Phellipe <slipkgao@hotmail.com>
2018-05-26 20:06:43 -0300

    create page login/nive acess in php/sesion/mysql

---
+++ usuarios.sql
@@ -0,0 +1,42 @@
+
+--
+-- Database: `exercicio_login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(220) NOT NULL,
+  `email` varchar(520) NOT NULL,
+  `senha` varchar(50) NOT NULL,
+  `situacoe_id` int(11) NOT NULL DEFAULT '0',
+  `niveis_acesso_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+--
+
+CREATE TABLE IF NOT EXISTS `niveis_acessos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `niveis_acessos`
+--
+
+INSERT INTO niveis_acessos (id, nome, created, modified) VALUES
+(1, 'Administrador', '2016-02-19 00:00:00', NULL),
+(2, 'Colaborador', '2016-02-19 00:00:00', NULL),
+(3, 'Cliente', '2016-02-19 00:00:00', NULL);
+-- --------------------------------------------------------
